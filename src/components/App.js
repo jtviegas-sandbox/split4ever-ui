5c87d294be63e4214516523d2687f2a5984b961a
joao tiago viegas <joao.viegas@maersk.com>
2020-09-28 16:26:43 +0200

    ...

--- src/components/App.js
+++ src/components/App.js
@@ -6,21 +6,40 @@ import Parts from './Parts';
 import Part from './Part';
 import Header from './Header';
 import Footer from './Footer';
+import DataService from "../services/data";
+import configuration from "../configuration";
 
-const App = () =>
-	<HashRouter>
-		<section className="container-fluid">
-			<Header />
-			<section className="container">
-				<Switch>
-					<Route path="/parts/:id" component={Part} />
-					<Route path="/parts" component={Parts} />
-					<Redirect from="/" to="/parts" /> } />
-				</Switch>
-			</section>
-			<Footer />
-		</section>
-	</HashRouter>
+
+class App extends React.Component {
+
+	constructor(props) {
+		super(props)
+		this.props = props;
+		this.data = new DataService(configuration);
+		this.state = {
+			items: {}
+		}
+	}
+
+	render(){
+		return (
+			<HashRouter>
+				<section className="container-fluid">
+					<Header />
+					<section className="container">
+						<Switch>
+							<Route path="/parts/:id" component={Part} />
+							<Route path="/parts" component={Parts} />
+							<Redirect from="/" to="/parts" /> } />
+						</Switch>
+					</section>
+					<Footer />
+				</section>
+			</HashRouter>
+		)
+	}
+
+}
 
 export default App;
 
